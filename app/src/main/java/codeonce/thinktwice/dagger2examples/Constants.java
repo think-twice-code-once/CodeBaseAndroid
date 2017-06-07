c6ad99458c752333838cdac8cd9be07f9d11626d
bss_PhongNguyen <phong.nguyen@beesightsoft.com>
2017-06-07 10:44:15 +0700

    Rearrange code and remove usused example.

--- app/src/main/java/codeonce/thinktwice/dagger2examples/Constants.java
+++
@@ -1,9 +0,0 @@
-package codeonce.thinktwice.dagger2examples;
-
-/**
- * -> Created by phong.nguyen@beesightsoft.com on 6/2/2017.
- */
-
-public class Constants {
-    public static final String LOG_TAG = "Coffee";
-}
--- app/src/main/java/dagger2/codebase/services/Value.java
+++
@@ -1,9 +0,0 @@
-package dagger2.codebase.services;
-
-/**
- * -> Created by phong.nguyen@beesightsoft.com on 6/2/2017.
- */
-
-public class Value {
-    public static final String LOG_TAG = "Football";
-}
